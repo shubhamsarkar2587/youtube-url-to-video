32e5d0a460f249b7adfcd4bab855522099e99567
Shubham <shubhamsarkar9654@gmail.com>
2024-01-20 04:34:07 +0530

    fixed cors error

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, send_file, jsonify
+from flask_cors import CORS
 from pytube import YouTube
 from io import BytesIO
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/download', methods=['POST'])
 def download_video():
